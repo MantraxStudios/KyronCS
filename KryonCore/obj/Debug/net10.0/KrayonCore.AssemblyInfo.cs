88a73c7e4aeedf8aeb92dac62ef9f103a5b2a4ca
MantraxStudios <tupapienrakion1234@gmail.com>
2026-02-08 23:03:41 -0300

    Bugs Fixed (Clone Only Mouse Hover Scene)

--- KryonCore/obj/Debug/net10.0/KrayonCore.AssemblyInfo.cs
+++ KryonCore/obj/Debug/net10.0/KrayonCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrayonCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caa47f6c2785edc0ed34174e5211bdb71b60bfd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09590e40ef2f60838d8af7cc0e8bd4e3b0ecf2b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrayonCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrayonCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
