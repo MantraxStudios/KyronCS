169e5ccae29ff6826e05b38f26f4a7ec8d3893bf
Josin <85538374+MantraxStudios@users.noreply.github.com>
2026-02-17 08:36:06 -0300

    Animation Example Scene

--- KrayonCompiler/obj/Debug/net10.0/KrayonCompiler.AssemblyInfo.cs
+++ KrayonCompiler/obj/Debug/net10.0/KrayonCompiler.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KrayonCompiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fee202d9c3dfcedaa3a5cd8d9fe605cc438b3a1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4f395fdc044ec723d97c6bf9dc3c9a08b31a5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("KrayonCompiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KrayonCompiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
